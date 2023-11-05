f407198ff4e51c1095e6dadbeb0f7797ddbbd9f9
agamy <zeyad.agamy@gmail.com>
2023-11-06 00:55:29 +0200

    initializing Commit

---
+++ GuessTheNumber/Guess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Guess
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            //int num = random.Next(1, 100);
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int num;
+            int guesses;
+            String resp;
+
+            while (playAgain)
+            {
+                num = random.Next(min, max+1);
+                guess = 0;
+                guesses=0;
+                while(guess != num)
+                {
+                    Console.WriteLine("Guess The Number between " + min + " - " + max);
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    if (guess > num )
+                    {
+                        max = guess;
+                        Console.WriteLine(guess + " ,is too High");
+                    }else if (guess < num)
+                    {
+                        min = guess;
+                        Console.WriteLine(guess + " ,is too Low");
+                    }
+                    guesses++;
+                }
+                Console.WriteLine("Win");
+                Console.WriteLine("The Number Was: " + num);
+                Console.WriteLine("Do You Want to Play Again? (Y/N) ");
+                resp = Convert.ToString(Console.ReadLine());
+                if(resp.ToUpper() == "Y")
+                {
+                    Console.WriteLine(" -----------------------");
+                    Console.WriteLine("|    Lets Play Again    |");
+                    Console.WriteLine(" -----------------------");
+                    playAgain = true;
+                    min = 1;
+                    max = 100;
+                }else if(resp.ToUpper() == "N")
+                {
+                    playAgain = false;
+                }
+             
+            }
+            Console.ReadKey();
+        }
+    }
+}
